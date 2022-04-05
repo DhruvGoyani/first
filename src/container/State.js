914c16b7f56d787397bc3257ea0cf0865cfe3413
DhruvGoyani <dhruvgoyani7@gmail,com>
2022-04-05 15:33:33 +0530

    compo

--- src/container/State.js
+++ src/container/State.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+// class base commponent
 class State extends Component {
     constructor(props) {
         super(props);
--- src/container/StateFun.js
+++ src/container/StateFun.js
@@ -1,18 +1,22 @@
-import React  from 'react';
-
+import React, { useState } from 'react';
+// function base commponent
 function StateFun(props) {
-    const [id , setId] = useState(101)
-
+    const [id, setId] = useState(101)
+    const [name, setName] = useState('')
 
     const change = () => {
         setId(102)
     }
     return (
         <>
-        <div>
-            {id}
-            <button onClick={() => change()}>change ID</button>
-        </div>
+            <div>
+                {id}
+                <button onClick={() => change()}>change ID</button>
+            </div>
+            <div>
+                {name}
+                <button onClick={() => setName('amit')}>change Name</button>
+            </div>
         </>
     );
 }
