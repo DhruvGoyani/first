cc8488d24bd243be0b13d0bbe914ddb90e65cd91
DhruvGoyani <dhruvgoyani7@gmail.com>
2022-04-11 08:47:39 +0530

    cnh

--- src/container/Clock.js
+++ src/container/Clock.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
 
 export default class Clock extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            time : new Date()
+        }
+        console.log(" 1 constructure")
+    }
   render() {
+    console.log(" 2 render");
     return (
-      <div>Clock</div>
+      <div>
+          
+          {this.state.time.totimestring()}
+      </div>
     )
   }
 }
