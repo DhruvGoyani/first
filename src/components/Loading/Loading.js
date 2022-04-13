25f2082099317841aaab4348755a04f1f9174ddc
DhruvGoyani <dhruvgoyani7@gmail.com>
2022-04-13 16:37:22 +0530

    cng

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // HOC
 export default function Loading(component) {
-    return function withLoadcomponent ({isLoading , data})
+    return function withLoadingcomponent ({isLoading , data})
     {
     if (isLoading) {
         return (
